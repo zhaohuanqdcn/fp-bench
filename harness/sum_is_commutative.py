93388fe933af06ba115189ec99e39b8f8c04fc3b
fouzhe <862006904@qq.com>
2025-04-12 16:11:00 +0800

    refactor(harness): ensure to flush for tempfile

--- harness/sum_is_commutative.py
+++ harness/sum_is_commutative.py
@@ -7,25 +7,22 @@ def execute_program(timeout: int) -> tuple[str, int]:
     # Create a temporary file
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
         temp_file.write("1.0 2.0 3.0 4.0 5.0")
+        temp_file.flush()
         temp_file_path = temp_file.name
 
-    try:
-        result = subprocess.run(
-            ["./program", temp_file_path],
-            capture_output=True,
-            encoding="utf-8",
-            errors="replace",
-            timeout=timeout,
-        )
-        # Return stderr and the return code
-        return result.stderr, result.returncode
-    except subprocess.TimeoutExpired as e:
-        # Timeout occurred, also ensure to return stderr captured before timeout and return code -signal.SIGKILL
-        return e.stderr, -signal.SIGKILL
-    except Exception as e:
-        # ensure to raise the error if run failed
-        raise e
-    finally:
-        # Clean up the temporary file
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
+        try:
+            result = subprocess.run(
+                ["./program", temp_file_path],
+                capture_output=True,
+                encoding="utf-8",
+                errors="replace",
+                timeout=timeout,
+            )
+            # Return stderr and the return code
+            return result.stderr, result.returncode
+        except subprocess.TimeoutExpired as e:
+            # Timeout occurred, also ensure to return stderr captured before timeout and return code -signal.SIGKILL
+            return e.stderr, -signal.SIGKILL
+        except Exception as e:
+            # ensure to raise the error if run failed
+            raise e
